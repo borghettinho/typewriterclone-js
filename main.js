c9281e7a55368a3bf74c1873825d2871ccdcbf17
Valker Vieira <valker.vieira@outlook.com>
2019-01-09 20:08:38 -0200

    Input, blink cursor and some minor details

---
+++ main.js
@@ -0,0 +1,7 @@
+const changeHandler = () => {
+    let inputText = document.querySelector('.inputText');
+    let outputText = document.querySelector('.outputText');
+
+    outputText.innerHTML = inputText.value;
+}
+
