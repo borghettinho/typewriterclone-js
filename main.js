8bb3a1e8c07f40c98aef14bbfaa4238aee85df2c
Valker Vieira <valker.vieira@outlook.com>
2019-01-12 10:31:00 -0200

    Corrigido o erro do não reconhecimento do espaço

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
 const changeHandler = () => {
     let inputText = document.querySelector('.inputText').value;
     let outputText = document.querySelector('.outputText');
-    let textContainer = inputText.split('');
+    let textContainer = [...inputText];
 
     outputText.innerText = '';
-    
-    let i = 0;
 
     textContainer.forEach((obj, index) => {
         setTimeout(() => {
-            outputText.innerText += obj
-        }, 200 * (index + 1));
+            outputText.textContent += obj;
+        }, 150 * (index));
     });
     
 }
